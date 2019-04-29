504890ae7a9e50db7a715c38e51a5794b334cdc3
erikagafe <erikagafe@gmail.com>
2019-04-29 16:55:51 +0200

    Add tests for Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,52 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+using namespace std;
 
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
 }
+
+
+TEST_CASE("vec2", "[Vec2]"){
+    Vec2 a;
+    Vec2 b{5.1f, -9.3f};
+    REQUIRE(a.x == 0.0f);
+    REQUIRE(a.y == 0.0f);
+    REQUIRE(b.x == 5.1f);
+    REQUIRE(b.y == -9.3f);
+};
+
+
+TEST_CASE("Vec2& operator+=", "[Vec2& operator+=]"){
+    Vec2 b{5.1f, -9.3f};
+    Vec2 a{5.1f, -9.3f};
+    a+=b;
+    REQUIRE(a.x== 10.2f);
+    REQUIRE(a.y == -18.6f);
+};
+
+TEST_CASE("Vec2& operator-=", "[Vec2& operator-=]"){
+    Vec2 b{5.1f, -9.3f};
+    Vec2 a{5.1f, -9.3f};
+    a-=b;
+    REQUIRE(a.x== 0.0f);
+    REQUIRE(a.y == 0.0f);
+};
+
+TEST_CASE("Vec2& operator*=", "[Vec2& operator*=]"){
+    Vec2 a{5.1f, -9.3f};
+    float b = 2.5;
+    a*=b;
+    REQUIRE(a.x== 12.75f);
+    REQUIRE(a.y == -23.25f);
+};
+
+TEST_CASE("Vec2& operator/=", "[Vec2& operator/=]"){
+    Vec2 a{5.1f, -9.3f};
+    float b = 1.3;
+    a/=b;
+    REQUIRE(a.x ==  Approx(3.9230).epsilon(0.1));
+    REQUIRE(a.y ==  Approx(-7.1538).epsilon(0.1));
+};
