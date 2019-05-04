8d7c5448c0607ca00934a1b68d5638487d12e218
erikagafe <erikagafe@gmail.com>
2019-05-05 00:03:24 +0200

    aufgabe 2.7

---
+++ source/color.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Erika on 5/4/19.
+//
+
+#ifndef BLATT2_COLOR_HPP
+#define BLATT2_COLOR_HPP
+
+
+
+
+struct Color{
+    float r = 0.45f;
+    float g = 0.45f;
+    floar b = 0.45f;
+
+};
+
+
+#endif //BLATT2_COLOR_HPP
