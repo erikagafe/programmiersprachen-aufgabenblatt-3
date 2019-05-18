9e2935c6df35e706ea41816621a0202139912cf3
erikagafe <erikagafe@gmail.com>
2019-05-18 17:23:39 +0200

    3.3

--- source/aufgabe_2_und_3.cpp
+++ source/aufgabe_2_und_3.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <list>
 #include <iterator>
+#include <map>
 #include<set>
 
 
@@ -15,22 +16,40 @@ int main()
 {
     std::list<unsigned int> listOfNumbers;
     std::set<unsigned int> setOfNumbers;
+    std::map<unsigned int, int> mapOfNumber;
+    std::map<unsigned int, int>::iterator itMap;
     for (int i = 0; i <100; i++){
         unsigned int j = rand() % 100;
         listOfNumbers.push_back(j);
-        setOfNumbers.insert(j);
+        itMap = mapOfNumber.find(j);
+        // Check if element exists in map or not
+        if (itMap != mapOfNumber.end()) {
+            unsigned int key = itMap->first;
+            int value = itMap->second;
+            mapOfNumber[key] = value + 1;
+
+        } else {
+            mapOfNumber[j] = 1;
+        }
 
     }
 
-    std::set<unsigned int>::iterator sit;
+
+
     std::list<unsigned int>::iterator it = listOfNumbers.begin();
     while(it != listOfNumbers.end())
     {
         std::cout<<(*it)<<"  ";
+        setOfNumbers.insert(*it);
         it++;
 
     }
+
+    std::cout<<std::endl;
     std::cout<< "set " <<std::endl;
+
+
+    std::set<unsigned int>::iterator sit;
     for (int i = 0; i <100; i++){
         sit = setOfNumbers.find(i);
         if(sit == setOfNumbers.end())
@@ -38,7 +57,18 @@ int main()
     }
 
 
-   return 0;
+    std::map<unsigned int, int>::iterator iteratorMap = mapOfNumber.begin() ;
+    while(iteratorMap != mapOfNumber.end())
+    {
+        unsigned int key = iteratorMap->first;
+        int value = iteratorMap->second;
+        std::cout << "key = " << key << " :: Value = " << value << std::endl;
+        iteratorMap ++;
+
+    }
+
+
+    return 0;
 
 
 }
